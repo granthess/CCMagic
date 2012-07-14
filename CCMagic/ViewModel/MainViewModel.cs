e2d56feae0195b25901e8c3e9f5c050ce28aa45c
Grant Hess <granthes@the-prof.net>
2012-07-14 00:48:46 -0600

    Wiring up MVVM test and updating license

--- CCMagic/ViewModel/MainViewModel.cs
+++ CCMagic/ViewModel/MainViewModel.cs
@@ -1,4 +1,23 @@
+/*
+    Copyright 2012, Grant Hess
+
+    This file is part of CC Magic.
+
+    S3ToolKit.Utils is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CC Magic.  If not, see <http://www.gnu.org/licenses/>.
+*/
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace CCMagic.ViewModel
 {
@@ -16,19 +35,43 @@ namespace CCMagic.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
+
+        public RelayCommand MyCommand { get; private set; }
+
+        
+        public string Data { get; set; }
+        public bool Enabled {get;private set;}
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
+        
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            MyCommand = new RelayCommand(() => DoSomething(), () => CheckSomething());
+            Enabled = true;
+
+            if (IsInDesignMode)
+            {
+                // Code runs in Blend --> create design time data.
+                Data = "Visible in design mode";
+            }
+            else
+            {
+                // Code runs "for real"
+                Data = "Visible in run mode";
+            }
+        }
+
+        private void DoSomething()
+        {
+            Data = Data + " Clicked";
+            Enabled = false;
+            RaisePropertyChanged("Data");            
+        }
+
+        private bool CheckSomething()
+        {
+            return Enabled;
         }
     }
 }
--- CCMagic/ViewModel/ViewModelLocator.cs
+++ CCMagic/ViewModel/ViewModelLocator.cs
@@ -1,15 +1,34 @@
 /*
-  In App.xaml:
-  <Application.Resources>
-      <vm:ViewModelLocator xmlns:vm="clr-namespace:CCMagic"
-                           x:Key="Locator" />
-  </Application.Resources>
-  
-  In the View:
-  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
+    Copyright 2012, Grant Hess
 
-  You can also use Blend to do all this with the tool's support.
-  See http://www.galasoft.ch/mvvm
+    This file is part of CC Magic.
+
+    S3ToolKit.Utils is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CC Magic.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+In App.xaml:
+<Application.Resources>
+    <vm:ViewModelLocator xmlns:vm="clr-namespace:CCMagic"
+                         x:Key="Locator" />
+</Application.Resources>
+  
+In the View:
+DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
+
+You can also use Blend to do all this with the tool's support.
+See http://www.galasoft.ch/mvvm
 */
 
 using GalaSoft.MvvmLight;
